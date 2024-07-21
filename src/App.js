1c8a1a673974182357f3b1f6159def57cfbffd5f
Gulnaz <viteewato@mail.ru>
2024-07-21 21:52:22 +0300

    mew_project without jsx

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
-import logo from './logo.svg';
+ import logo from './logo.svg';
+//import React from 'react';
+import { createElement } from 'react';
 import './App.css';
 
-function getYear() {                              // императивный
+function getYear() {
   let date = new Date();
   let year = date.getFullYear()
-  return (<p>{year}</p>)
+  return createElement('p',null, {year})
 }
 
 
-export const App = () => {                   // декларативный
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          Year  {getYear()}
-        </p>
-      </header>
-    </div>
-  );
+export const App = () => {
+		return createElement('div',createElement('header', {className: 'App-header'},
+												createElement('img', {className : 'App-logo',src : {logo}}, null),
+												createElement ('p',null,'Edit',
+												createElement('code',null, 'src/App.js' ), 'and save to reload.'),
+												createElement('a',{className: 'App-link',href:'https://reactjs.org', target:'_blank', rel: 'noopener noreferrer'  }, 'Learn React'),
+												getYear,
+												null)
+												,{className : 'App'}, null)
 }
+
 
 
 
